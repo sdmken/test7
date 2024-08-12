63f9cb8b9d64cb761c9a4062495abaac80fa6017
sdmken <116410449+sdmken@users.noreply.github.com>
2024-08-12 09:12:01 +0900

    Initial Commit

---
+++ test7UITests/test7UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test7UITestsLaunchTests.swift
+//  test7UITests
+//
+//  Created by 堤健二 on 2024/08/12.
+//
+
+import XCTest
+
+final class test7UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
